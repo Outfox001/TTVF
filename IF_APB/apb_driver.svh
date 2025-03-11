db015fafd7c10d7f792db400db4c5fdc02fba878
TeodoraBalga <126147545+TeodoraBalga@users.noreply.github.com>
2025-03-11 16:42:55 +0200

    Add files via upload

--- IF_APB/apb_driver.svh
+++ IF_APB/apb_driver.svh
@@ -1,11 +1,3 @@
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Module name: afvip_apb_driver
-// HDL        : UVM
-// Author     : Paulovici Vlad-Marian
-// Description: UVM driver is an active entity that has knowledge on how to drive signals to a particular interface of the design.
-//              Is the place where the protocols are realized to be tested.
-// Date       : 28 August, 2023
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class afvip_apb_driver extends uvm_driver #(afvip_apb_item, afvip_apb_item);
 
   `uvm_component_utils(afvip_apb_driver)
@@ -24,20 +16,24 @@ class afvip_apb_driver extends uvm_driver #(afvip_apb_item, afvip_apb_item);
   endfunction
 
   virtual task run_phase(uvm_phase phase);
-    zero();
     @(vif.cb_master);
     @(posedge vif.rst_n);
+
+    vif.cb_master.psel 			                        <=  0;
+    vif.cb_master.enable			                      <=  0;
 
 	  forever begin    
     	seq_item_port.get_next_item(req);
       $cast (data_project, req.clone());
       data_project.set_id_info(req);
-    	repeat(data_project.delay_psel) @(vif.cb_master);   
-    	case (data_project.pwrite)
-        1'b0:read(data_project);
-        1'b1:write(data_project);
-      endcase
 
+      vif.cb_master.psel 			                        <=  1;
+       @(vif.cb_master);
+      vif.cb_master.penable		                        <=  1;
+       @(vif.cb_master);
+      vif.cb_master.psel 			                        <=  0;
+      vif.cb_master.enable			                      <=  0;
+      
 	    seq_item_port.item_done();
       seq_item_port.put_response(data_project);
       `uvm_info (get_type_name(), $sformatf ("Data was received in APB_DRIVER"), UVM_NONE)
@@ -46,37 +42,10 @@ class afvip_apb_driver extends uvm_driver #(afvip_apb_item, afvip_apb_item);
     
   endtask
 
-  virtual task zero ();
-    vif.cb_master.psel 			                        <=0;
-    vif.cb_master.pwrite                            <=0;
-    vif.cb_master.penable                           <=0;
-    vif.cb_master.paddr                             <=0;
-    vif.cb_master.pwdata                            <=0;
-  endtask
 
-  virtual task write( afvip_apb_item data_project);
-    vif.cb_master.psel 			                        <=  1;
-    vif.cb_master.pwrite                            <=  data_project.pwrite;  
-    vif.cb_master.pwdata 		                        <=  data_project.pwdata;
-    vif.cb_master.paddr 		                        <=  data_project.paddr;
-    @(vif.cb_master);                       
-  	vif.cb_master.penable 	                        <=  1;
-    @(vif.cb_master iff vif.cb_master.pready);
-    vif.cb_master.penable 	                        <=  0;
-    vif.cb_master.psel 			                        <=  0;    
-  endtask
 
-  virtual task read(inout afvip_apb_item data_project);
-    vif.cb_master.psel 			                        <=  1;
-    vif.cb_master.pwrite                            <=  data_project.pwrite;
-    vif.cb_master.paddr 		                        <=  data_project.paddr;
-    @(vif.cb_master);                       
-  	vif.cb_master.penable 	                        <=  1;
-    @(vif.cb_master iff vif.cb_master.pready);
-    data_project.prdata                             =   vif.cb_master.prdata; 
-    vif.cb_master.penable 	                        <=  0;
-    vif.cb_master.psel 			                        <=  0;
-  endtask
 
+
+  
 
 endclass //req_ack_driver extends uvm_driver
